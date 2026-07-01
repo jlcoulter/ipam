1f1e69fb5de6662782d023994acf38146d24b6bb
jlcoulter <coulter.l.jack@gmail.com>
2026-07-01 21:07:06 +1000

    subnet list and add cobra cmd

--- subnet.go
+++ subnet.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var subnetCmd = &cobra.Command{
 	Use:   "subnet",
@@ -25,8 +30,18 @@ var subnetAddCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		cidr := args[0]
 		name, _ := cmd.Flags().GetString("name")
+		desc, _ := cmd.Flags().GetString("desc")
+		vlan, _ := cmd.Flags().GetInt("vlan")
+
+		ipamSvc, dbObj := mustOpenIPAM()
+		defer dbObj.Close()
+
 		// TODO: insert subnet into DB
-		_, _ = cidr, name
+		if err := ipamSvc.AddSubnet(cidr, name, desc, vlan); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Printf("subnet %s added\n", cidr)
 	},
 }
 
@@ -35,6 +50,24 @@ var subnetListCmd = &cobra.Command{
 	Short: "List all registered subnets",
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: list all subnets
+		ipamSvc, dbObj := mustOpenIPAM()
+		defer dbObj.Close()
+
+		subnets, err := ipamSvc.Subnets()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		if len(subnets) == 0 {
+			fmt.Println("no subnets registered ")
+			return
+		}
+
+		fmt.Printf("%-4s %-20s %-15s %-5s %-20s\n", "ID", "CIDR", "Name", "VLAN", "Created")
+		for _, s := range subnets {
+			fmt.Printf("%-4d %-20s %-15s %-5d %-20s\n", s.ID, s.CIDR, s.Name, s.VLAN, s.CreatedAt)
+		}
 	},
 }
 
@@ -44,3 +77,4 @@ func init() {
 	subnetAddCmd.Flags().IntP("vlan", "v", 0, "VLAN ID")
 	subnetCmd.AddCommand(subnetShowCmd, subnetAddCmd, subnetListCmd)
 }
+
